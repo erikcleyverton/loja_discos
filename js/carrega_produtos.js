8b9c5e8383005f60c06490900875c8a7992ba0be
Erik Cleyverton <erik.c.correia@aluno.senai.br>
2026-07-07 21:00:26 -0300

    adicionei o js

--- js/carrega_produtos.js
+++ js/carrega_produtos.js
@@ -1,10 +1,35 @@
 import {produtos} from"./produtos.js"; 
 
-//PEGANDO ELEMENTOS DO DOM
-const ListProdutos = () =>{
-produtosForeach((elem, i)=>{
+const section_cards = document.querySelector('#cards');
 
+const listarProdutos = () => {
+    section_cards.innerHTML = '';
 
-})
+    produtos.forEach((elem, i) => {
+        const divCard = document.createElement('div');
+        divCard.setAttribute('class', 'card');
 
-}
+        const imgProduto = document.createElement('img');
+        imgProduto.setAttribute('src', elem.caminho_da_imagem);
+        imgProduto.setAttribute('alt', elem.descricao_produto);
+        imgProduto.setAttribute('class', 'img_card');
+
+        const h2Titulo = document.createElement('h2');
+        h2Titulo.innerHTML = elem.descricao_produto;
+
+        const h3Valor = document.createElement('h3');
+        divValor.setAttribute('class', 'valor_card');
+        divValor.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.', ',')}`;
+
+        const btnCard = document.createElement('button');
+        btnCard.setAttribute('class', 'btn_card');
+        btnCard.innerHTML = 'Adicionar';
+
+        divCard.appendChild(imgProduto);
+        divCard.appendChild(h2Titulo);
+        divCard.appendChild(divValor);
+        divCard.appendChild(btnCard);
+
+        section_cards.appendChild(divCard);
+    });
+};
