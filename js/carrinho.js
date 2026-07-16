c853a2a3e0f56a2c52cabdbd8c82990aaf9ff484
Noite Cetafaju <erik.c.correia@aluno.senai.br>
2026-07-16 19:55:17 -0300

    mudei icon da loja e teste de java

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,47 +1,47 @@
-// CRIANDO ARRAY DE ITENS CARRINHO 
-const itensCarrinho = (JSON.parse(localStorage.getItem('itensSessao')) || []).filter(elem => elem !== null)
+// CRIANDO O ARRAY DE ITENS DO CARRINHO
+const itensCarrinho = JSON.parse(localStorage.getItem('itensSessao')) || []
 
-
-// CRIANDO ARROW ITEM 
-const objItem = (objProduto) => {
- const Item = {
-    id_produto: 2,
-    descricao_produto: "Madonna – Confessions 2",
-    caminho_da_imagem: "../../imagens/Confessions2.jpg",
-    valor_unitario: 360.00,
-    quantidade: 1
- }
-
- return Item
+// CRIANDO ARROW ITEM (Igual ao do professor)
+const fObjItem = (objProduto) => {
+    const item = {
+        id_produto: objProduto.id_produto,
+        descricao_produto: objProduto.descricao_produto,
+        caminho_da_imagem: objProduto.caminho_da_imagem,
+        valor_unitario: objProduto.valor_unitario,
+        quantidade: 1
+    }
+    return item
 }
 
+// PEGANDO O INDICE DO ARRAY
+console.log("índice do array ->>> ", itensCarrinho.findIndex(elem => elem && elem.id_produto == 12))
 
-// PEGANDO O INDICE DO ARRAY 
-console.log("indice do array ->>> ", itensCarrinho.findIndex(elem => elem?.id_produto == 12))
+// FUNÇÃO PARA ADICIONAR O ITEM NO ARRAY (Estrutura do professor com controle de quantidade)
+const addItem = (objItem) => {
+    // 1 Verificamos se o produto já existe no carrinho
+    const index = itensCarrinho.findIndex(elem => elem && elem.id_produto == objItem.id_produto)
 
-
-// FUNÇÃO PARA ADICIONAR O ITEM NO ARRAY
-const addItem = (itemAdicionado) => {
-    itensCarrinho.push(itemAdicionado)
+    if (index > -1) {
+        // 2 Se já existe, olhamos ele no array e somamos +1 na quantidade 📈
+        itensCarrinho[index].quantidade += 1
+    } else {
+        // 3. Se não existe, criamos o item usando fObjItem e adicionamos normalmente 🆕
+        itensCarrinho.push(fObjItem(objItem))
+    }
 
     localStorage.setItem('itensSessao', JSON.stringify(itensCarrinho))
 }
 
-
 // LISTAR ITENS DO CARRINHO
 const listItens = () => {
-    const itensSelecionado = JSON.parse(localStorage.getItem('itensSessao')) || []
-
-    return itensSelecionado.filter(elem => elem !== null)
+    const itensSelecionados = JSON.parse(localStorage.getItem('itensSessao')) || []
+    return itensSelecionados
 }
-
 
 // REMOVER ELEMENTO
 const removeItem = (pos) => {
     itensCarrinho.splice(pos, 1)
-
     localStorage.setItem('itensSessao', JSON.stringify(itensCarrinho))
 }
-
 
 export { addItem, listItens, removeItem };
